d73828627992aee44bbc1ff33a1a0073fdb294a5
Hubert Yang <offspring7890@yahoo.com>
2019-11-18 13:49:24 -0800

    get request functional

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -4,15 +4,39 @@ import LanguageService from '../../services/language-service';
 import './Dashboard.css';
 
 class DashboardRoute extends Component {
+
+  static contextType = LanguageContext;
+
+  showCards = () => {
+    let cards = document.getElementById('practiceCards');
+    let quiz = document.getElementById('quizCards')
+    cards.style.display = 'block';
+    quiz.style.display = 'none';
+  }
+
+  showQuiz = () => {
+    let quiz = document.getElementById('quizCards')
+    let cards = document.getElementById('practiceCards');
+    quiz.style.display = 'block';
+    cards.style.display = 'none';
+  }
+
+  async componentDidMount() {
+    const {language, words} =  await LanguageService.getLanguage()
+    
+    this.context.setScore(language.total_score)
+  }
+
   render() {
+    let score = this.context.score;
     return (<>
       <body>
 
         <main>
           <div className='upperSection'>
-            <p className='currentScore'>Your overall score:&nbsp;7</p> 
-            <button className="basicBtn btnB" onclick='showCards()'>Study Mode</button>
-            <button className="basicBtn btnB" onclick='showQuiz()'>Quiz Mode</button>
+            <p className='currentScore'>Your overall score:{score}</p> 
+            <button className="basicBtn btnB" onclick={this.showCards}>Study Mode</button>
+            <button className="basicBtn btnB" onclick={this.showQuiz}>Quiz Mode</button>
           </div>
         </main>
       </body>

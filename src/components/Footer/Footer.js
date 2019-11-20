f7f152206eb8ff109d1ddf597cbf517daa2d981c
laura <laura.j.elias@gmail.com>
2019-11-20 10:23:10 -0600

    corrected footer settings

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 
 export default function Footer(){
     return ( 
-        <footer>
+        <footer className='mainFooter'>
             <p className='footerText'>Created by <a className='footerLink' href='https://thinkful-ei-gecko.github.io/portfolio-wireframe-badri/' target='_blank' rel='noopener noreferrer'>Badri Narayana Tulsiram</a> || <a className='footerLink' href='https://flashcabaja64.github.io/portfolio/' target='_blank' rel='noopener noreferrer'>Hubert Yang</a> || Laura Elias</p>
         </footer>
     )

0e2e403ffb6a0de69a0d38de92491cbac4bc1d58
Badri-narayan <badritulsiram@gmail.com>
2019-11-20 07:59:58 -0800

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 export default function Footer(){
     return ( 
         <footer>
-            Created by <a className='footerLink' href='https://thinkful-ei-gecko.github.io/portfolio-wireframe-badri/' target='_blank' rel='noopener noreferrer'>Badri Narayana Tulsiram</a> || <a className='footerLink' href='https://flashcabaja64.github.io/portfolio/' target='_blank' rel='noopener noreferrer'>Hubert Yang</a> || Laura Elias
+            <p className='footerText'>Created by <a className='footerLink' href='https://thinkful-ei-gecko.github.io/portfolio-wireframe-badri/' target='_blank' rel='noopener noreferrer'>Badri Narayana Tulsiram</a> || <a className='footerLink' href='https://flashcabaja64.github.io/portfolio/' target='_blank' rel='noopener noreferrer'>Hubert Yang</a> || Laura Elias</p>
         </footer>
     )
 }

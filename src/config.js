0d2290e87bab1abf0a1ace66d2b46dcc3834cca5
laura <laura.j.elias@gmail.com>
2019-11-22 15:24:58 -0600

    updated server link

--- src/config.js
+++ src/config.js
@@ -1,9 +1,6 @@
 const config = {
   API_ENDPOINT: 'http://localhost:8000/api',
-  REACT_APP_API_BASE:
-    process.env.NODE_ENV === 'production'
-      ? 'https://learn-spanish-med-terms-api.herokuapp.com'
-      : 'http://localhost:8000/api'
+  REACT_APP_API_BASE: 'https://learn-spanish-med-terms-api.herokuapp.com'
 }
 
 export default config;

139eee56a080af25e3d8e66545365b2bb5b7a82f
laura <laura.j.elias@gmail.com>
2019-11-24 10:19:44 -0600

    link added to footer

---
+++ build/precache-manifest.c22507069755c100964ac0a804477026.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39196d332cbb8c36c6cc",
+    "url": "/static/css/main.754dd9f5.chunk.css"
+  },
+  {
+    "revision": "39196d332cbb8c36c6cc",
+    "url": "/static/js/main.39196d33.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a1694c3278568f8bc8a1",
+    "url": "/static/js/2.a1694c32.chunk.js"
+  },
+  {
+    "revision": "e38b9db1b33cab5fbc72f137574fa31f",
+    "url": "/index.html"
+  }
+];
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,9 @@ import './Footer.css'
 export default function Footer(){
     return ( 
         <footer className='mainFooter'>
-            <p className='footerText'>Created by <a className='footerLink' href='https://thinkful-ei-gecko.github.io/portfolio-wireframe-badri/' target='_blank' rel='noopener noreferrer'>Badri Narayana Tulsiram</a> || <a className='footerLink' href='https://flashcabaja64.github.io/portfolio/' target='_blank' rel='noopener noreferrer'>Hubert Yang</a> || Laura Elias</p>
+            <p className='footerText'>Created by <a className='footerLink' href='https://thinkful-ei-gecko.github.io/portfolio-wireframe-badri/' target='_blank' rel='noopener noreferrer'>Badri Narayana Tulsiram</a>
+             || <a className='footerLink' href='https://flashcabaja64.github.io/portfolio/' target='_blank' rel='noopener noreferrer'>Hubert Yang</a>
+             || <a className='footerLink' href='https://ljelias.github.io/LEportfolio/' target='_blank' rel='noopener noreferrer'>Laura Elias</a></p>
         </footer>
     )
 }
